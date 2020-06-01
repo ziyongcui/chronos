abd1f87f42a2f1a9d7af84f38e83b719008118b0
Ziyong Cui <ziyongcuii@gmail.com>
2020-06-01 16:44:04 -0400

    first commit of blank project

---
+++ chronos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chronos
+//
+//  Created by Ziyong Cui on 6/1/20.
+//  Copyright © 2020 Ziyong Cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chronosTests/chronosTests.swift
@@ -0,0 +1,34 @@
+//
+//  chronosTests.swift
+//  chronosTests
+//
+//  Created by Ziyong Cui on 6/1/20.
+//  Copyright © 2020 Ziyong Cui. All rights reserved.
+//
+
+import XCTest
+@testable import chronos
+
+class chronosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
