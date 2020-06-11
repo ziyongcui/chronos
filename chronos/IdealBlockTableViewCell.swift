780979921098e5abfdd6360c0aec28ef0e9ee22b
Connor Dang <c.dang@berkeley.edu>
2020-06-11 09:22:40 -0700

    Added TableView and prototype cell to storyboard

---
+++ chronos/IdealBlockTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IdealBlockTableViewCell.swift
+//  chronos
+//
+//  Created by Ronnoc Gnad on 6/11/20.
+//  Copyright © 2020 Ziyong Cui. All rights reserved.
+//
+
+import UIKit
+
+class IdealBlockTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
