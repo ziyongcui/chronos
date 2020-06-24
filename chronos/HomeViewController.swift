53f1fbed6d386e28d2074f8bada8fddae16c1019
Connor Dang <c.dang@berkeley.edu>
2020-06-24 00:01:48 -0700

    fixed color glitch on dark mode for home screen, major storyboard changes, implemented tab bar, prototyped 2 other popup menus

--- chronos/HomeViewController.swift
+++ chronos/HomeViewController.swift
@@ -2,25 +2,19 @@
 //  HomeViewController.swift
 //  chronos
 //
-//  Created by Ronnoc Gnad on 6/4/20.
+//  Created by Ronnoc Gnad on 6/23/20.
 //  Copyright © 2020 Ziyong Cui. All rights reserved.
 //
 
 import UIKit
-import SwiftEntryKit
-class HomeViewController: UIViewController, Storyboarded {
-    @IBAction func help(_ sender: Any) {
-        guard let view = self.view as? HomeView else {
-            fatalError("WelcomeViewController doesn't have a WelcomeView as its view.")
-        }
-        view.showTour()
-    }
 
-    @IBAction func skipTour(_ sender: Any) {
-        guard let view = self.view as? HomeView else {
-            fatalError("WelcomeViewController doesn't have a WelcomeView as its view.")
-        }
-        view.fadeTour()
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
     }
     
 
